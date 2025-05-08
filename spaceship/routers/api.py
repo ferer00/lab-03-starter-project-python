d9dc1cb7612e12ef38b3c4514d4dc7fbc091e81b
ferer00 <ferer004829@gmail.com>
2025-05-08 20:03:26 +0300

    Added endpoint

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,23 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
-
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/matrix")
+async def matrix_mult():
+    # Генерація матриць
+    matrix_a = np.random.rand(10, 10).tolist()
+    matrix_b = np.random.rand(10, 10).tolist()
+    
+    # Множення матриць
+    product = np.dot(matrix_a, matrix_b).tolist()
+    
+    return {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product
+    }
